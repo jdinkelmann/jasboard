c80efff53aafa2750091aee4d29fc8e4dca0be2e
Jason Dinkelmann <jason@elev8ed.io>
2025-12-17 23:30:00 -0500

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import Weather from '../components/widgets/Weather';
-import Calendar from '../components/widgets/Calendar';
-import Photos from '../components/widgets/Photos';
-import Metar from '../components/widgets/Metar';
+import Default from './themes/default';
 
 /**
  * JasBoard Main Display
@@ -11,25 +8,6 @@ import Metar from '../components/widgets/Metar';
  */
 export default function Home() {
   return (
-    <main className="min-h-screen w-full p-4">
-      {/* Portrait layout: 1080x1920 */}
-      <div className="max-w-[1080px] mx-auto h-screen flex flex-col gap-4">
-        {/* Weather at top */}
-        <section className="flex-shrink-0">
-          <Weather />
-        </section>
-
-        {/* Calendar in middle - takes most space */}
-        <section className="flex-grow overflow-hidden">
-          <Calendar />
-        </section>
-
-        {/* Bottom section: Photos and METAR side by side */}
-        <section className="flex-shrink-0 grid grid-cols-2 gap-4 h-64">
-          <Photos />
-          <Metar />
-        </section>
-      </div>
-    </main>
+    <Default />
   );
 }
